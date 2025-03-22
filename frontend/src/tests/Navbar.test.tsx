c706086d335db9b6f39a125c4a9c603acd763416
Cindy Tosin Ariyo <ariyoc@tcd.ie>
2025-03-22 20:45:33 +0000

    Cleaned Navbar.test.tsx

--- frontend/src/tests/Navbar.test.tsx
+++ frontend/src/tests/Navbar.test.tsx
@@ -22,20 +22,17 @@ describe('Navbar Component', () => {
     });
   });
 
-  // it('changes opacity on hover', () => {
-  //   render(<Navbar />);
+  it('changes opacity on hover', () => {
+    render(<Navbar />);
     
-  //   const productsLink = screen.getByRole('link', { name: /Products/i });
+    const productsLink = screen.getByRole('link', { name: /Products/i });
   
-  //   // Check if the initial opacity class is applied (if you use opacity-80 for the initial state)
-  //   expect(productsLink).toHaveClass('opacity-80');
+    expect(productsLink).toHaveClass('opacity-80');
     
-  //   // Simulate hover over the link and check for opacity change (opacity-100 or similar class for hover)
-  //   fireEvent.mouseEnter(productsLink);
-  //   expect(productsLink).toHaveClass('opacity-100');
+    fireEvent.mouseEnter(productsLink);
+    expect(productsLink).toHaveClass('opacity-100');
     
-  //   // Simulate mouse leave and check for opacity reset to opacity-80
-  //   fireEvent.mouseLeave(productsLink);
-  //   expect(productsLink).toHaveClass('opacity-80');
-  // });
+    fireEvent.mouseLeave(productsLink);
+    expect(productsLink).toHaveClass('opacity-80');
+  });
 });
